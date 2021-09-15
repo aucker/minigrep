d1f3c17303811912e0efe2ea0ec796d6a7e78f24
aucker <aucker@qq.com>
2021-09-15 23:14:26 +0800

    fix the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-//grep: Globally search a Regular Expression and Print
+/*//grep: Globally search a Regular Expression and Print
 
 use std::env;
 //use std::env::args; //return an iterator of the command line arguments that were given to minigrep
 //the std::env::args will panic if any argument contains invalid Unicode
-use std::fs;    //handle files
+//use std::fs;    //handle files
 use std::process;
-
+use minigrep::Config;
 fn main() {
+
 
     let args: Vec<String> = env::args().collect();
 
@@ -24,21 +25,21 @@ fn main() {
     });
 
     //println!("the test is {}", test);
-    println!("searching for {}", config.query);
-    println!("In file {}", config.filename);
+   // println!("searching for {}", config.query);
+   // println!("In file {}", config.filename);
     //println!("{:?}", config.args);
     //println!("Hello, world!");
 
-    let contents = fs::read_to_string(config.filename).expect("something went wrong reading the file");
-    //open the file, return the Result<String> of the file's contents
-    println!("with text:\n{}", contents);
+    //let contents = fs::read_to_string(config.filename).expect("something went wrong reading the file");
+    // open the file, return the Result<String> of the file's contents
+    //println!("with text:\n{}", contents);
 }
 
-struct Config {
+/*struct Config {
     query: String,
     filename: String,
 }
-
+*/
 /*impl Config {
     fn new(args: &[String]) -> Config {
         if args.len() < 3 {
@@ -51,7 +52,7 @@ struct Config {
     }
 }*/
 
-impl Config {
+/*impl Config {
     fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -63,10 +64,35 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
-
-/*fn parse_config(args: &[String]) -> Config {
+*/
+fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let filename = args[2].clone();
 
     Config { query, filename }
 }*/
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
